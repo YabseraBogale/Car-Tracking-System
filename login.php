5e1d04f5636f15fdb77089c53a8c551b3c494bdc
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-04 09:13:27 +0300

    ok

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<div class="login-wrap">
+  <h2>Login</h2>
+  
+  <div class="form">
+    <input type="text" placeholder="Username" name="un" />
+    <input type="password" placeholder="Password" name="pw" />
+    <input type="submit" value="Sign in">
+    <a href="#"> <p> Don't have an account? Register </p></a>
+  </div>
+</div>
+<script src="login.js"></script>
+</body>
+</html>
