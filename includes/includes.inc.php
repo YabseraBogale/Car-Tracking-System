bc47354151a88ff74553ef90dd772b8db32728ab
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-06 20:35:31 +0300

    ok

---
+++ includes/includes.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+spl_autoload_register('load');
+function load($class){
+    $path ='classes';
+    $extension='.class.php';
+    $file= $path.$class.$extension;
+    if(!file_exists($file)){
+        return false;
+    }
+    include_once $file;
+}
+
+?>
