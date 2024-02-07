49a5a2b6ea6cf5c539d514894f98a329e19a6c10
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-07 16:08:04 +0300

    ok

--- classes/Model.class.php
+++ classes/Model.class.php
@@ -1,54 +1,29 @@
 <?php 
 class Model extends Database{
     protected function CreateUser($firstname,$lastname,$username,$email,$password){
-        $statment="INSERT INTO  User(firstname,lastname,username,email,password) values(?,?,?,?,?);";
-        $command=$this->OpenConnection()->prepare($statment);
-        $password=password_hash($password,PASSWORD_DEFAULT);
-        $stmt->bind_param("sssss",$firstname,$lastname,$username,$email,$password);
-        $stmt->execute();
+
+        $statment="INSERT INTO  User(firstname,lastname,username,email,password) values('$firstname','$lastname','$username','$email','$password');";
+        $this->OpenConnection()->query($statment);
+       
     }
     protected function Location($lat,$long){
-        $statment="INSERT into Location(lat,lon) values(?,?)";
-        $command=$this->OpenConnection()->prepare($statment);
-        $stmt->bind_param("ss",$lat,$long);
-        if($stmt->execute()){
-            return "location entered";
-        } else {
-            return "try again";
-        }
+        $statment="INSERT into Location(lat,lon) values($lat,$$long)";
+        $this->OpenConnection()->query($statment);
     }
     protected function AddCar($platenumber,$carimage){
-        $statment="INSERT into Car(platenumber,carimage) values(?,?)";
-        $command=$this->OpenConnection()->prepare($statment);
-        $stmt->bind_param("ss",$platenumber,$carimage);
-        if($stmt->execute()){
-            return "Added Car";
-        } else {
-            return "try again";
-        }
+        $statment="INSERT into Car(platenumber,carimage) values('$platenumber','$carimage')";
+        $this->OpenConnection()->query($statment);
     }
     protected function RemoveCar($platenumber){
-        $statment="Delete from CAR Where platenumber=?";
-        $command=$this->OpenConnection()->prepare($statment);
-        $stmt->bind_param("s",$platenumber);
-        if($stmt->execute()){
-            return "you have removed car";
-        } else {
-            return "try again";
-        }
+        $statment="Delete from CAR Where platenumber='$platenumber'";
+        $this->OpenConnection()->query($statment);
     }
     protected function AdminSeeAllUser(){
         return $this->OpenConnection()->query("Select username,password from user");
     }
     protected function AdminRemoveUser($id){
-        $statment="Delete from User Where id=?";
-        $command=$this->OpenConnection()->prepare($id);
-        $stmt->bind_param("i",$id);
-        if($stmt->execute()){
-            return "you have removed user";
-        } else {
-            return "try again";
-        }
+        $statment="Delete from User Where id=$id";
+        $this->OpenConnection()->query($statment);
     }
     protected function AdminAll(){
         return $this->OpenConnection()->query("Select * from Admin");
